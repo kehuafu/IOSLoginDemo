b57939829173e8c256cf56960f03cd337d4c2f0e
khf <1593238887@qq.com>
2021-01-05 11:57:44 +0800

    [feature][登录模块]：新增登录界面

--- LoginDemo/ViewController.m
+++ LoginDemo/ViewController.m
@@ -8,6 +8,10 @@
 #import "ViewController.h"
 
 @interface ViewController ()
+@property (strong, nonatomic) IBOutlet UITextField *numberText;
+@property (strong, nonatomic) IBOutlet UITextField *passwordText;
+- (IBAction)login:(id)sender;
+@property (strong, nonatomic) IBOutlet UIButton *loginButon;
 
 @end
 
@@ -16,7 +20,26 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+    [self.numberText addTarget:self action:@selector(textChange)forControlEvents:(UIControlEventEditingChanged)];
+    
+    [self.passwordText addTarget:self action:@selector(textChange)forControlEvents:(UIControlEventEditingChanged)];
+
+
 }
-
-
+- (void)textChange{
+    NSLog(@"%@",self.numberText.text);
+    NSLog(@"%@",self.passwordText.text);
+    if(self.numberText.text.length>0&self.passwordText.text.length>0){
+        self.loginButon.enabled = YES;
+    }else{
+        self.loginButon.enabled = NO;
+    }
+}
+//- (BOOL)textField:(UITextField *)textField shouldChangeCharactersInRange:(NSRange)range replacementString:(NSString *)string{
+//    NSLog(@"%@",self.numberText.text);
+//    return YES;
+//}
+- (IBAction)login:(id)sender {
+    NSLog(@"123456");
+}
 @end
